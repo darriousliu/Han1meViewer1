72ca2756dba910d43036f5f7de56e1271fa80043
Darrious Liu <60057825+darriousliu@users.noreply.github.com>
2026-08-21 00:12:43 -0700

    refactor(db): DatabaseRepo 进 commonMain

--- shared/src/commonMain/kotlin/io/github/daisukikaffuchino/han1meviewer/logic/DatabaseRepo.kt
+++ shared/src/commonMain/kotlin/io/github/daisukikaffuchino/han1meviewer/logic/DatabaseRepo.kt
@@ -14,6 +14,7 @@ import io.github.daisukikaffuchino.han1meviewer.logic.entity.WatchHistoryEntity
 import io.github.daisukikaffuchino.han1meviewer.logic.entity.download.DownloadGroupEntity
 import io.github.daisukikaffuchino.han1meviewer.logic.entity.download.HanimeDownloadEntity
 import io.github.daisukikaffuchino.han1meviewer.logic.model.SearchOption
+import kotlin.jvm.JvmOverloads
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
